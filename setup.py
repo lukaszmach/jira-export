523c7f21d1504995bd81c080b1e5e95a60fd029b
Łukasz Machowski <149838491+lukaszmach@users.noreply.github.com>
2024-10-07 17:02:33 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,13 +6,22 @@ here = pathlib.Path(__file__).parent.resolve()
 
 long_description = (here / "README.md").read_text(encoding="utf-8")
 
+import pathlib
+
+from setuptools import find_packages, setup
+
+here = pathlib.Path(__file__).parent.resolve()
+
+long_description = (here / "README.md").read_text(encoding="utf-8")
+
 setup(
     name='jira_export',
-    version='0.0.1',
+    version='0.0.2',
     description='Small program for exporting issues from Jira to pdf or html',
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/lukaszmach/jira-export",
+    download_url = 'https://github.com/lukaszmach/jira-export/archive/refs/tags/0.0.2.tar.gz',  
     author='Łukasz Machowski',
     author_email='macherek@o2.pl',
     packages=find_packages(),
@@ -21,5 +30,12 @@ setup(
         'jira',
         'pdfkit',
         'pypandoc-binary',
+        'keyboard'
     ],
+    entry_points={
+        'console_scripts': [
+            'jira_export=jira_export:main',
+        ],
+    },
 )
+
